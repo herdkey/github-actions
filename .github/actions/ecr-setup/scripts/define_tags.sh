23d7e1ed948b639df88ac287a9be2417020b995c
Michael Knopf <michael@herdkey.com>
2025-09-21 11:24:17 -0700

    readme

--- .github/actions/ecr-setup/scripts/define_tags.sh
+++ .github/actions/ecr-setup/scripts/define_tags.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Base URL of the ECR tags
-IMAGE_BASE="$REGISTRY/$ENV_NAME/$REPO_SLUG/api"
+IMAGE_BASE="$REGISTRY/$ENV_NAME/$REPO_SLUG/$COMPONENT_NAME"
 
 # take the first 7 chars of the full commit SHA (shorter, still unique enough for dev)
 SHORT_SHA=$(echo "$GITHUB_SHA" | cut -c1-7)
@@ -14,9 +14,11 @@ SANITIZED_BRANCH="${GITHUB_REF_NAME//\//-}"
 # Lowercase the branch name (bash parameter expansion: ${var,,})
 SANITIZED_BRANCH="${SANITIZED_BRANCH,,}"
 
-# Export vars to later GitHub Actions steps by appending to the special file $GITHUB_ENV.
-echo "SHORT_SHA=$SHORT_SHA" >> "$GITHUB_ENV"
-echo "SANITIZED_BRANCH=$SANITIZED_BRANCH" >> "$GITHUB_ENV"
-echo "IMAGE_BASE=$IMAGE_BASE"
-echo "BRANCH_TAG=${IMAGE_BASE}:${SANITIZED_BRANCH}"
-echo "SHA_TAG=${IMAGE_BASE}:${SHORT_SHA}"
+# Export vars as step outputs by appending to the special file $GITHUB_OUTPUT.
+{
+    echo "SHORT_SHA=$SHORT_SHA"
+    echo "SANITIZED_BRANCH=$SANITIZED_BRANCH"
+    echo "IMAGE_BASE=$IMAGE_BASE"
+    echo "BRANCH_TAG=${IMAGE_BASE}:${SANITIZED_BRANCH}"
+    echo "SHA_TAG=${IMAGE_BASE}:${SHORT_SHA}"
+} >> "$GITHUB_OUTPUT"
